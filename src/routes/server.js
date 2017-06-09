5829a18f93c6e762b20ebc56e435d315cf704656
itditp <mikhailbilida@gmail.com>
2017-06-09 14:28:16 +0300

    done

--- src/routes/server.js
+++ src/routes/server.js
@@ -7,41 +7,54 @@ var PDFDocument = require('pdfkit');
 var doc = new PDFDocument();
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '198989',
+    user: '---',
+    password: '---',
     database: 'nyble'
 });
-/*
-var user = {
+
+/*var user = {
     image: fs.readFileSync('./public/images/pPPYjUlUjf8.jpg'),
-    firstName: 'Dog',
+    firstName: 'Fog',
     lastName: 'Cat'
-};
-*/
-connection.connect();
-/*connection.query('INSERT INTO user SET ?', user, function(err,
+};*/
+/*connection.query('INSERT INTO user SET ?', user, function (err,
     result) {
     console.log(result);
 });*/
+connection.connect();
 router.route('/')
     .post(function (req, res) {
         var name = {
             firstName: req.body.firstName
         };
+        console.log(name.firstName);
         connection.query('SELECT * FROM user WHERE firstName = ?', name.firstName, function (error, results, fields) {
-            var answer = {};
+            var answer;
+
             if (error) {
                 throw error;
             }
+
             if (results.length >= 1) {
                 console.log('user exists');
                 var userId = results[0].id;
-                var w = doc.pipe(fs.createWriteStream('out.pdf'));
+                var stream = doc.pipe(fs.createWriteStream('out.pdf'));
 
-                w.on('close', function () {
+                doc.fontSize(14)
+                    .text(results[0].firstName + ' ' + results[0].lastName, 100, 80);
+                doc.image(new Buffer(results[0].image), {
+                    fit: [250, 300],
+                    align: 'center',
+                    valign: 'center'
+                });
+                doc.save();
+                doc.end();
+
+                stream.on('finish', function () {
                     var pdfFile = {
                         pdf: fs.readFileSync('./out.pdf')
                     };
+                    console.log(pdfFile.pdf);
                     connection.query('UPDATE user SET pdf = ? WHERE id = ?', [pdfFile.pdf, userId], function (error, results, fields) {
                         if (error) {
                             throw error;
@@ -52,15 +65,6 @@ router.route('/')
                     };
                     res.json(answer);
                 });
-                doc.fontSize(14)
-                    .text(results[0].firstName + ' ' + results[0].lastName, 100, 80);
-                doc.image(new Buffer(results[0].image), {
-                    fit: [250, 300],
-                    align: 'center',
-                    valign: 'center'
-                });
-                doc.save();
-                doc.end();
             }
 
             if (results.length === 0) {
